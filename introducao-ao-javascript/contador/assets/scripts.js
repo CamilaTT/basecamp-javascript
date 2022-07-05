fedeac0f9298eb9ee2e88df131ab2bd8cbf832e7
Camila <camila99pessoa@gmail.com>
2022-07-05 01:10:19 -0300

    adicionado mecanismo para alterar a cor do número atual para vermelho quando for negativo

--- introducao-ao-javascript/contador/assets/scripts.js
+++ introducao-ao-javascript/contador/assets/scripts.js
@@ -5,25 +5,31 @@ const BTN_INCREMENT = document.getElementById('adicionar');
 const BTN_DECREMENT = document.getElementById('subtrair');
 
 function increment() {
-
 	if(count < 10) {
 		count++;
-		console.log(count)
 		CURRENT_NUMBER.innerHTML = count;
+		changeColor()
 	}
 }
 
 function decrement() {
-
 	if(count > -10) {
 		count--;
-		console.log(count)
 		CURRENT_NUMBER.innerHTML = count;
+		changeColor()
 	}
 }
 
 function test() {
 	kdowkdpo;
+}
+
+function changeColor() {
+	if(count >= 0) {
+		CURRENT_NUMBER.classList.remove('negativeNumber')
+	} else {
+		CURRENT_NUMBER.classList.add('negativeNumber')
+	}
 }
 
 BTN_INCREMENT.addEventListener('click', increment);
